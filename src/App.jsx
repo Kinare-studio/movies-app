acae5d72649b4dfea05ba03a1fd81977ce327b1d
Regina Arefinkina <regi.kolyadina@yandex.ru>
2023-08-22 17:39:26 +0300

    chore: установи Material-UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import { AppRoutes } from "./routes/AppRoutes";
 
-export default function App() {
+export function App() {
   return (
     <Router>
       <AppRoutes />
