34f6e4b98e3181565034b35a3a5b16da685c9c81
Regina Arefinkina <regi.kolyadina@yandex.ru>
2023-08-22 20:24:24 +0300

    feat: добавь разметку и стили для шапки сайта

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { AppRoutes } from "./routes/AppRoutes";
 import { Header } from "./components/Header";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export function App() {
   return (
-    <Router>
-      <Header />
-      <AppRoutes />
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <AppRoutes />
+      </Router>
+    </ThemeProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,56 @@
 import React from "react";
-import { AppBar, Container, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+  Avatar,
+} from "@mui/material";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 
 export function Header() {
   return (
-    <AppBar>
-      <Container>
-        <Toolbar />
-      </Container>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ marginX: "40px" }}>
+          <IconButton
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              marginRight: "20px",
+            }}
+          >
+            <Avatar
+              sx={{
+                background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+                marginRight: "10px",
+              }}
+            >
+              <OndemandVideoIcon />
+            </Avatar>
+            <Typography variant="h6" component="div">
+              Movies App
+            </Typography>
+          </IconButton>
+          <Box sx={{ marginLeft: "auto" }}>
+            <Button color="inherit" variant="outlined">
+              Log In
+            </Button>
+          </Box>
+          <Button
+            sx={{
+              color: "white",
+              background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+              marginLeft: "10px",
+            }}
+            variant="contained"
+          >
+            Sign Up
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
