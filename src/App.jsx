071041c228010c908eac5d9f8130cc8a1380a20e
Regina Arefinkina <regi.kolyadina@yandex.ru>
2023-08-22 18:35:59 +0300

    chore: установи зависимости MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
     <Router>
+      <Header />
       <AppRoutes />
     </Router>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppBar, Container, Toolbar } from "@mui/material";
+
+export function Header() {
+  return (
+    <AppBar>
+      <Container>
+        <Toolbar />
+      </Container>
+    </AppBar>
+  );
+}
