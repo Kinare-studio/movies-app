d7703dc0445cae442941d3071ec08a75a460d3fe
Regina Arefinkina <regi.kolyadina@yandex.ru>
2023-08-23 13:52:49 +0300

    refactor: удали лишнее

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,6 @@ import { FilmList } from "../components/FilmList";
 import "../css/main.css";
 
 export function Home() {
-  // const url = "https://api.kinopoisk.dev/v1.3/movie";
-  // const apiKey = "RT1MBCK-0C94DBW-H12TZGT-PDZ2410";
-
   return (
     <div className="home">
       <SearchBar />
